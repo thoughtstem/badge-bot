1ecbc3a730c6c7b70205b279ac519cdeb5121302
Lindsey Handley <lindseyhandley@gmail.com>
2020-05-19 19:48:59 +0000

    badge bot now lets users see list of badges and award badges to users

---
+++ badges.rkt
@@ -0,0 +1,49 @@
+#lang racket
+
+(require 2htdp/image
+	 discord-bot
+	 gregor)
+
+(provide badges
+	 (struct-out badge)
+	 badge-img-with-id
+	 award-badge!)
+
+(struct badge (id name url img))
+
+(define (badges)
+  (list 
+    (badge 'hw1 "Hello World 1" "https://forum.metacoders.org/t/badge-hello-world-1/108" (circle 40 'solid 'red))
+    (badge 'hw2 "Hello World 2" "https://forum.metacoders.org/t/badge-hello-world-1/108" (circle 40 'solid 'blue))
+    (badge 'hw3 "Hello World 3" "https://forum.metacoders.org/t/badge-hello-world-1/108" (circle 40 'solid 'green))
+	))
+
+(define (badge-img-with-id b)
+  (overlay (text (~a (badge-id b)) 24 'black)
+	   (badge-img b)))
+
+(define (badge-id? id)
+  (member id (map badge-id (badges))))
+
+(define/contract (award-badge! badge-id user)
+  (-> badge-id? is-mention? boolean?)
+  
+  (define (badge-earning badge-id)
+    (list
+      badge-id
+      (moment->iso8601/tzid (now/moment #:tz "America/Los_Angeles"))
+      (messaging-user-name)
+      ))
+  (define val
+    (session-load user 'earned `()))
+
+  (when (member badge-id (map first val))
+    (error (~a user " already has that badge!")))
+
+  (set! val   
+    (cons
+      (badge-earning badge-id)
+      val))
+
+  (session-store user 'earned val)
+  #t)
--- main.rkt
+++ main.rkt
@@ -1,11 +1,49 @@
 #lang racket
 
-(require discord-bot)
-(require discourse-bot)
+(require discord-bot
+	 discourse-bot
+	 "badges.rkt")
 
+(define (badges-command . args)
+  (define sub-command-name (first args))
+
+  (define sub-command
+    (match sub-command-name 
+	   ["list" list-badges-command]
+	   ["award" award-badges-command]))
+
+  (apply sub-command (rest args))
+  )
+
+(define (list-badges-command sub-command-name)
+  (define sub-command
+    (match sub-command-name
+	   ["images" list-badge-images-command]
+	   ["names" list-badge-names-command]))
+  (sub-command))
+
+(define (list-badge-images-command)
+  (define (show-badge b)
+	  (badge-img-with-id b))
+  (map show-badge (badges)))
+
+(define (list-badge-names-command)
+  (define (show-badge b)
+    (list (~a (badge-id b)
+	      " "
+	      (badge-name b)
+	      ": " 
+	      (badge-url b))))
+  (map show-badge (badges)))
+
+(define (award-badges-command badge-id user)
+  (award-badge! (string->symbol badge-id) user)
+  (~a "You've awarded " badge-id " to " user "!"))
 
 (define b
   (bot
-    ["hello" (thunk* "world")]))
+    ["hello" (thunk* "world")]
+    ["badges" badges-command]
+    ))
 
 (launch-bot b)
